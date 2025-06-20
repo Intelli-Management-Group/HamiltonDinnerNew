812bef5009c5dce65e60f10612070bcdc265821f
smit-intellidt <smitd@intellidt.com>
2025-06-20 18:50:14 +0530

    api update

--- app/Http/Controllers/Api/DinningController.php
+++ app/Http/Controllers/Api/DinningController.php
@@ -5252,18 +5252,13 @@ class DinningController extends Controller
 
         $categoryId = $request->input('categoryId');
 
-
-        
-        
-        
         try{
-
-            // item_details left join category_details on item_details.cat_id = category_details.id my 
 
             $items = ItemDetail::select('item_details.*', 'category_details.parent_id')
                 ->leftJoin('category_details', 'item_details.cat_id', '=', 'category_details.id')
                 ->where('item_details.deleted_on', NULL)
                 ->where('category_details.deleted_on', NULL)
+                ->where('item_details.cat_id', $categoryId)
                 ->get();
 
             $results = [];
@@ -5271,7 +5266,8 @@ class DinningController extends Controller
             foreach ($items as $item){
 
                 $results[] = [
-                    'type' => 'sub_cat_item',
+                    'type' => empty($item->parent_id) ? 'item' : 'sub_cat_item',
+                    
                     'item_name' => $item->item_name,
                     'item_id' => $item->id,
                     'preference' => $item->preference,
