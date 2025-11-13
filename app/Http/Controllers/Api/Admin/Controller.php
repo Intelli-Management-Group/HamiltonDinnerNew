b367e7069589020188f6e54596c010b5343b1dd7
Keenan Chan <keenan@Keenans-Mac-mini.local>
2025-11-12 21:42:36 -0800

    Add sendResultJSON to Controller

--- app/Http/Controllers/Api/Admin/Controller.php
+++ app/Http/Controllers/Api/Admin/Controller.php
@@ -9,4 +9,24 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    var $success_code = 200;
+
+    /**
+     * Method to return result json
+     * @param Boolean $status = true if success,false if error
+     * @param String $msg = Success/Error message
+     * @param Array $data = Array containing data to return
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResultJSON($status, $msg = null, $data = array())
+    {
+        $return_data = array(
+            'ResponseCode' => $status,
+            'ResponseText' => $msg
+        );
+        foreach ($data as $key => $value) {
+            $return_data[$key] = $value;
+        }
+        return response()->json($return_data, $this->success_code);
+    }
 }
